551d6b985326530798cec800430f96fa51e97ac8
Rita <zeta@widcket.com>
2017-06-01 10:54:58 -0300

    Added the Browser model

---
+++ admin/app/Browser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Browser extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Get the ratings that belong to this browser.
+     */
+    public function ratings()
+    {
+        return $this->hasMany('App\Rating');
+    }
+}
