3f2613f62316541e4ebf202213ec1f227e712484
Rita <zeta@widcket.com>
2017-06-01 14:51:25 -0300

    Completed Brand migrations

--- admin/database/migrations/2017_06_01_135536_create_brands_table.php
+++ admin/database/migrations/2017_06_01_135536_create_brands_table.php
@@ -15,6 +15,7 @@ class CreateBrandsTable extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name', 30);
             $table->timestamps();
         });
     }
