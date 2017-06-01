5e50fd29408c8fe5d608c7b7feb4c9e71c63df97
Rita <zeta@widcket.com>
2017-06-01 10:35:43 -0300

    Added the App model

--- admin/app/App.php
+++ admin/app/App.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class App extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'type', 'modified_by'
+    ];
+
+    /**
+     * Get the user that last modified the App.
+     */
+     public function modifiedBy() {
+        return $this->belongsTo('App\User', 'modified_by');
+     }
 }
