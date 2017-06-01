f637627b366d1da15d6815ec7c995628c35f681b
Rita <zeta@widcket.com>
2017-06-01 10:56:39 -0300

    Added the Brand model

---
+++ admin/app/Brand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Get the devices that belong to this brand.
+     */
+    public function devices()
+    {
+        return $this->hasMany('App\Device');
+    }
+}
