2f62db0ba02cd20631bceb774cf0c937836eba22
Rita <zeta@widcket.com>
2017-06-01 15:30:05 -0300

    Removed duplicated routes

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -15,22 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
-
-
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
