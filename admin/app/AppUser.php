859afc0e6224f6777a8bcb7fab51093b7aeadb0a
Rita <zeta@widcket.com>
2017-06-01 10:24:23 -0300

    Added AppUser Model

--- admin/app/AppUser.php
+++ admin/app/AppUser.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class AppUser extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email',
+    ];
 }
