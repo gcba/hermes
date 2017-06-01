fe1a32d04476005c2cb21ffbe1c69455061a16cf
Rita <zeta@widcket.com>
2017-06-01 13:50:43 -0300

    Completed App migration

--- admin/database/migrations/2017_05_30_144303_create_apps_table.php
+++ admin/database/migrations/2017_05_30_144303_create_apps_table.php
@@ -15,7 +15,11 @@ class CreateAppsTable extends Migration
     {
         Schema::create('apps', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name', 50);
+            $table->char('type', 1);
             $table->timestamps();
+            $table->integer('modified_by')->unsigned()->nullable();
+            $table->foreign('modified_by')->references('id')->on('users')->onDelete('set null');
         });
     }
 
