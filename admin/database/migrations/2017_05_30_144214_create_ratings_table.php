573955441a19b1b9a382b7989ce291007b4ba668
Rita <zeta@widcket.com>
2017-06-01 13:44:22 -0300

    Completed Rating migrations

--- admin/database/migrations/2017_05_30_144214_create_ratings_table.php
+++ admin/database/migrations/2017_05_30_144214_create_ratings_table.php
@@ -14,7 +14,23 @@ class CreateRatingsTable extends Migration
     public function up()
     {
         Schema::create('ratings', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
+            $table->smallInteger('rating');
+            $table->string('description', 30)->nullable();
+            $table->string('app_version', 15)->nullable();
+            $table->string('platform_version', 15);
+            $table->string('browser_version', 15)->nullable();
+            $table->boolean('has_message');
+            $table->integer('app_id')->unsigned();
+            $table->foreign('app_id')->references('id')->on('apps')->onDelete('cascade');
+            $table->integer('appuser_id')->unsigned();
+            $table->foreign('appuser_id')->references('id')->on('appusers')->onDelete('cascade');
+            $table->integer('platform_id')->unsigned()->nullable();
+            $table->foreign('platform_id')->references('id')->on('platforms')->onDelete('set null');
+            $table->integer('device_id')->unsigned()->nullable();
+            $table->foreign('device_id')->references('id')->on('devices')->onDelete('set null');
+            $table->integer('browser_id')->unsigned()->nullable();
+            $table->foreign('browser_id')->references('id')->on('browsers')->onDelete('set null');
             $table->timestamps();
         });
     }
