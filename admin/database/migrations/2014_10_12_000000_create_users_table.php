a4460fa06137013ddca2f89ea1f7a1db973b7fdf
Rita <zeta@widcket.com>
2017-06-01 13:08:51 -0300

    Added onDelete() in User migrations

--- admin/database/migrations/2014_10_12_000000_create_users_table.php
+++ admin/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->foreign('modified_by')->references('id')->on('users');
+            $table->foreign('modified_by')->references('id')->on('users')->onDelete('set null');
         });
     }
 
