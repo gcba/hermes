ba3294d945cdf86d8be17eb1b0305b2ece18e339
Rita <zeta@widcket.com>
2017-06-01 11:03:46 -0300

    Added the Rating model

--- admin/app/Rating.php
+++ admin/app/Rating.php
@@ -6,5 +6,62 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'rating',
+        'description',
+        'app_version',
+        'platform_version',
+        'browser_version',
+        'has_message',
+        'app_id',
+        'appuser_id',
+        'platform_id',
+        'device_id',
+        'browser_id',
+    ];
+
+    /**
+     * Get the app the rating belongs to.
+     */
+    public function app()
+    {
+        return $this->belongsTo('App\App');
+    }
+
+    /**
+     * Get the app user the rating belongs to.
+     */
+    public function appUser()
+    {
+        return $this->belongsTo('App\AppUser', 'appuser_id');
+    }
+
+    /**
+     * Get the platform the rating belongs to.
+     */
+    public function platform()
+    {
+        return $this->belongsTo('App\Platform');
+    }
+
+    /**
+     * Get the device the rating belongs to.
+     */
+    public function device()
+    {
+        return $this->belongsTo('App\Device');
+    }
+
+    /**
+     * Get the browser the rating belongs to.
+     */
+    public function browser()
+    {
+        return $this->belongsTo('App\Browser');
+    }
 }
