cb3fe7fccd635689ef2d21236a26ffc641099bd8
Rita <zeta@widcket.com>
2017-06-01 12:36:41 -0300

    Completed Users migration

--- admin/database/migrations/2014_10_12_000000_create_users_table.php
+++ admin/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('modified_by')->references('id')->on('users');
         });
     }
 
