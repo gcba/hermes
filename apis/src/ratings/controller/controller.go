405d9ee30fb130b644c17679a0d3b172b728772e
Rita <zeta@widcket.com>
2017-06-05 13:58:35 -0300

    Made db connection logic environment-aware

--- apis/src/ratings/controller/controller.go
+++ apis/src/ratings/controller/controller.go
@@ -1,11 +1,25 @@
 package controller
 
 import (
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite" // sqlite driver
 )
 
 func connectDB() *gorm.DB {
+	env := os.Getenv("API_RATINGS_ENV")
+
+	if env != "PRODUCTION" {
+		db, err := gorm.Open("../../../../admin/database", "database.sqlite")
+		if err != nil {
+			panic("Failed to connect to database")
+		}
+		defer db.Close()
+
+		return db
+	}
+
 	db, err := gorm.Open("../../../../admin/database", "database.sqlite")
 	if err != nil {
 		panic("Failed to connect to database")
