feaf10eb775f0f043fb5ea76eb0fd768d7f23fa7
Rita <zeta@widcket.com>
2017-06-05 17:37:46 -0300

    Configured different read/write db servers

--- admin/config/database.php
+++ admin/config/database.php
@@ -41,7 +41,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'read' => [
+                'host' => env('DB_HOST', '127.0.0.1'),
+            ],
+            'write' => [
+                'host' => env('DB_HOST', '127.0.0.1')
+            ],
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
