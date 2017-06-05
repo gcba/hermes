ea1409e03dd2af6973f161c1f72f01cd91c2d0ea
Rita <zeta@widcket.com>
2017-06-05 11:27:48 -0300

    Added Rating model struct

--- apis/src/ratings/models/rating.go
+++ apis/src/ratings/models/rating.go
@@ -7,4 +7,16 @@ import (
 
 type Rating struct {
     gorm.Model
+
+    Rating int8
+    Description string `gorm:"size:30"`
+    AppVersion string `gorm:"size:15"`
+    PlatformVersion string `gorm:"size:15"`
+    BrowserVersion string `gorm:"size:15"`
+    HasMessage bool
+    AppID int
+    AppUserID int `gorm:"column:appuser_id"`
+    PlatformID int
+    DeviceID int
+    BrowserID int
 }
