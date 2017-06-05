fb60b91cfb36175c4b136c0fea510011a4b1e05e
Rita <zeta@widcket.com>
2017-06-05 11:37:28 -0300

    Added AppUser model struct

--- apis/src/ratings/models/appuser.go
+++ apis/src/ratings/models/appuser.go
@@ -7,4 +7,12 @@ import (
 
 type AppUser struct {
     gorm.Model
+
+    Name string `gorm:"size:70"`
+    Email string `gorm:"size:100"`
+}
+
+// set AppUser's table name to be `appuser`
+func (AppUser) TableName() string {
+  return "appuser"
 }
