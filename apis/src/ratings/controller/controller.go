a3797316f2698a0a82b5b4d249f67ee4cc7adab8
Rita <zeta@widcket.com>
2017-06-05 13:08:21 -0300

    Added basic db connection logic

--- apis/src/ratings/controller/controller.go
+++ apis/src/ratings/controller/controller.go
@@ -1,1 +1,14 @@
 package controller
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite" // sqlite driver
+)
+
+func connectDB() {
+	db, err := gorm.Open("../../../../admin/database", "database.sqlite")
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	defer db.Close()
+}
