de320b9f1cf3202a9df56a82708608a8a2869cde
Rita <zeta@widcket.com>
2017-06-07 15:38:31 -0300

    Created AppUsers seeds

---
+++ admin/database/seeds/AppUsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\AppUser;
+
+class AppUsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (AppUser::count() == 0) {
+            AppUser::create([
+                'name'           => 'Mariano Gómez',
+                'email'          => 'mariano@gomez.com'
+            ]);
+
+            AppUser::create([
+                'name'           => 'Esteban Sosa',
+                'email'          => 'esteban@sosa.com'
+            ]);
+
+            AppUser::create([
+                'name'           => 'Germán Álvarez',
+                'email'          => 'german@alvarez.com'
+            ]);
+        }
+    }
+}
