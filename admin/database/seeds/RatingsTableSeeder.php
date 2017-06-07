13837486378774c8c4b8bc8aab2461bc8358423c
Rita <zeta@widcket.com>
2017-06-07 18:50:01 -0300

    Refactored Rating seeds

--- admin/database/seeds/RatingsTableSeeder.php
+++ admin/database/seeds/RatingsTableSeeder.php
@@ -22,56 +22,47 @@ class RatingsTableSeeder extends Seeder
             $ios = Platform::where('name', 'iOS')->firstOrFail();
             $android = Platform::where('name', 'Android')->firstOrFail();
 
-            for ($i = 0; $i < 5; $i++) {
-                switch ($i) {
-                    case 0:
-                        $description = 'Muy malo';
-                        break;
-                    case 1:
-                        $description = 'Malo';
-                        break;
-                    case 2:
-                        $description = 'Regular';
-                        break;
-                    case 3:
-                        $description = 'Bueno';
-                        break;
-                    case 4:
-                        $description = 'Muy Bueno';
-                        break;
-                }
+            foreach ($apps as $app) {
+                foreach ($appusers as $appuser) {
+                    $rating = array_rand([1, 2, 3, 4, 5]);
+                    $description;
 
-                 foreach ($apps as $app) {
-                    foreach ($appusers as $appuser) {
-                        $willLeaveRating = array_rand([true, false, true]);
-
-                        if ($willLeaveRating) {
-                            $description;
-                            $deviceId;
-                            $hasMessage = array_rand([true, false]);
-                            $platform = array_rand(['iOS', 'Android']);
-                            $platformId = $platform == "iOS" ? $ios->id : $android->id;
-                            $platformVersion = $platform == "iOS" ?
-                                array_rand(['8.0', '9.0']) :
-                                array_rand(['5.1', '6.0']);
-                            $devices = $platform == "iOS" ?
-                                array_rand($ios->devices()) :
-                                array_rand($android->devices());
-                            $deviceId = array_rand($devices)->id;
-
-                            Rating::create([
-                                'rating' => $i + 1,
-                                'description' => $description,
-                                'app_version' => array_rand(['1.0', '2.0']),
-                                'platform_version' => $platformVersion,
-                                'has_message' => $hasMessage,
-                                'app_id' => $app->id,
-                                'appuser_id' => $appuser->id,
-                                'platform_id' => $platformId,
-                                'device_id' => $deviceId
-                            ]);
-                        }
+                    switch ($rating) {
+                        case 1:
+                            $description = 'Muy malo';
+                            break;
+                        case 2:
+                            $description = 'Malo';
+                            break;
+                        case 3:
+                            $description = 'Regular';
+                            break;
+                        case 4:
+                            $description = 'Bueno';
+                            break;
+                        case 5:
+                            $description = 'Muy Bueno';
+                            break;
                     }
+
+                    $hasMessage = array_rand([true, false]);
+                    $platform = array_rand(['iOS', 'Android']);
+                    $platformId = $platform == "iOS" ? $ios->id : $android->id;
+                    $platformVersion = $platform == "iOS" ? array_rand(['8.0', '9.0']) : array_rand(['5.1', '6.0']);
+                    $devices = $platform == "iOS" ? array_rand($ios->devices()) : array_rand($android->devices());
+                    $deviceId = array_rand($devices)->id;
+
+                    Rating::create([
+                        'rating' => $i,
+                        'description' => $description,
+                        'app_version' => array_rand(['1.0', '2.0']),
+                        'platform_version' => $platformVersion,
+                        'has_message' => $hasMessage,
+                        'app_id' => $app->id,
+                        'appuser_id' => $appuser->id,
+                        'platform_id' => $platformId,
+                        'device_id' => $deviceId
+                    ]);
                 }
             }
         }
