93219e37ba7a6a8da6f481965cdf18417ad32db7
Rita <zeta@widcket.com>
2017-06-07 18:44:47 -0300

    Fixed code syntax issues

--- admin/database/seeds/Apps_Platforms_UsersSeeder.php
+++ admin/database/seeds/Apps_Platforms_UsersSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use TCG\Voyager\Models\User;
+use TCG\Voyager\Models\Role;
 use App\App;
 use App\Platform;
 use App\AppUser;
@@ -47,40 +47,33 @@ class Apps_Platforms_UsersSeeder extends Seeder
 
             // Let's attach Apps to Platforms
 
-            $ios.apps()->attach($denunciaVial->id);
-            $ios.apps()->attach($miBa->id);
-            $ios.apps()->attach($masSimple->id);
+            $ios->apps()->attach($denunciaVial->id);
+            $ios->apps()->attach($miBa->id);
+            $ios->apps()->attach($masSimple->id);
 
-            $android.apps()->attach($denunciaVial->id);
-            $android.apps()->attach($miBa->id);
-            $android.apps()->attach($masSimple->id);
+            $android->apps()->attach($denunciaVial->id);
+            $android->apps()->attach($miBa->id);
+            $android->apps()->attach($masSimple->id);
 
             // Let's attach AppUsers to Platforms
 
-            $appuser1 = AppUser::where('email', 'mariano@gomez.com')->firstOrFail();
-            $appuser2 = AppUser::where('email', 'esteban@sosa.com')->firstOrFail();
-            $appuser3 = AppUser::where('email', 'german@alvarez.com')->firstOrFail();
-            $appuser4 = AppUser::where('email', 'mariela@dominguez.com')->firstOrFail();
-            $appuser5 = AppUser::where('email', 'juliana@perez.com')->firstOrFail();
-            $appuser6 = AppUser::where('email', 'valentina@echeverria.com')->firstOrFail();
+            $appusers = AppUser::all();
 
-            $ios.appusers()->attach($appuser1->id);
-            $ios.appusers()->attach($appuser2->id);
-            $ios.appusers()->attach($appuser4->id);
+            $ios->appusers()->attach($appuser[0]->id);
+            $ios->appusers()->attach($appuser[1]->id);
+            $ios->appusers()->attach($appuser[3]->id);
 
-            $android.appusers()->attach($appuser3->id);
-            $android.appusers()->attach($appuser5->id);
-            $android.appusers()->attach($appuser6->id);
+            $android->appusers()->attach($appuser[2]->id);
+            $android->appusers()->attach($appuser[4]->id);
+            $android->appusers()->attach($appuser[5]->id);
 
             // Let's attach Users to Apps
 
-            $support1 = User::where('email', 'juan@fernandez.com')->firstOrFail();
-            $support2 = User::where('email', 'martina@gimenez.com')->firstOrFail();
-            $support3 = User::where('email', 'paula@carrizo.com')->firstOrFail();
+            $supportUsers = Role::where('name', 'support')->firstOrFail()->users();
 
-            $support1.apps()->attach($denunciaVial->id);
-            $support2.apps()->attach($miBa->id);
-            $support3.apps()->attach($masSimple->id);
+            $supportUsers[0]->apps()->attach($denunciaVial->id);
+            $supportUsers[1]->apps()->attach($miBa->id);
+            $supportUsers[2]->apps()->attach($masSimple->id);
         }
     }
 }
