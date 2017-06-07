6de66d21f0a3b961b3b9924dd8505b5065ccb7cf
Rita <zeta@widcket.com>
2017-06-07 19:18:59 -0300

    Added Message seeds

--- admin/database/seeds/MessagesTableSeeder.php
+++ admin/database/seeds/MessagesTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Message;
+use App\Rating;
 
 class MessagesTableSeeder extends Seeder
 {
@@ -11,6 +13,19 @@ class MessagesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        if (Message::count() == 0) {
+            $ratingsWithMessages = Rating::where('has_message', true)->findOrFail();
+
+            foreach ($ratingsWithMessages as $rating) {
+                $message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed eiusmod tempor incidunt ';
+                $message . 'ut labore et dolore magna aliqua.';
+
+                Message::create([
+                    'message' => $message,
+                    'direction' => 'in',
+                    'rating_id' => $rating->id
+                ]);
+            }
+        }
     }
 }
