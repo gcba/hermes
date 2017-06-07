5bf207ba7e4a6cf038c4cfa7d929af7f2615e787
Rita <zeta@widcket.com>
2017-06-07 19:47:22 -0300

    Fixed table name

--- admin/app/AppUser.php
+++ admin/app/AppUser.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AppUser extends Model
 {
-    protected $table = 'appuser';
+    protected $table = 'appusers';
 
     /**
      * The attributes that are mass assignable.
