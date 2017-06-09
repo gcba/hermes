71fdf29cf23e7bd6be590da1cfa5ddd880bdf2ed
Rita <zeta@widcket.com>
2017-06-09 18:50:58 -0300

    Added Brand creation test

--- apis/src/ratings/models/brand_test.go
+++ apis/src/ratings/models/brand_test.go
@@ -1,1 +1,24 @@
 package models
+
+import (
+	"ratings/controller"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateBrand(t *testing.T) {
+	db := controller.GetWriteDB()
+	defer db.Close()
+
+	brand := Brand{Name: "Test"}
+	result := db.Create(&brand)
+
+	require.Equal(t, nil, result.Error)
+
+	if value, ok := result.Value.(*Brand); ok {
+		require.Equal(t, "Test", value.Name)
+	} else {
+		t.Fatal("Value is not a Brand")
+	}
+}
