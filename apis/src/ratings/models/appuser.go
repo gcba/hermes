19cc452c54a8085801f24ecd4af927673288e3fa
Rita <zeta@widcket.com>
2017-06-13 20:48:30 -0300

    Added AppUser test

--- apis/src/ratings/models/appuser.go
+++ apis/src/ratings/models/appuser.go
@@ -5,16 +5,15 @@ import (
 )
 
 type AppUser struct {
-	ID     uint   `gorm:"primary_key"`
+	ID     uint   `gorm:"primary_key;AUTO_INCREMENT"`
 	Name   string `gorm:"size:70;not null"`
 	Email  string `gorm:"size:100;not null"`
 	MiBAID uint   `gorm:"column:miba_id;not null"`
 
 	CreatedAt time.Time `gorm:"not null"`
-	UpdatedAt time.Time
 }
 
 // TableName sets AppUser's table name to be `appuser`
 func (AppUser) TableName() string {
-	return "appuser"
+	return "appusers"
 }
--- apis/src/ratings/models/appuser_test.go
+++ apis/src/ratings/models/appuser_test.go
@@ -1,1 +1,57 @@
 package models
+
+import (
+	"ratings/controller"
+	"testing"
+
+	"github.com/dchest/uniuri"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAppUser(t *testing.T) {
+	db := controller.GetWriteDB()
+	defer db.Close()
+
+	name := uniuri.New()
+	email := "test@test.com"
+	mibaID := uint(2)
+
+	appuser := AppUser{Name: name, Email: email, MiBAID: mibaID}
+	result := db.Create(&appuser)
+
+	require.Equal(t, nil, result.Error)
+
+	if value, ok := result.Value.(*AppUser); ok {
+		require.Equal(t, name, value.Name)
+		require.Equal(t, email, value.Email)
+		require.Equal(t, mibaID, value.MiBAID)
+	} else {
+		t.Fatal("Value is not an AppUser")
+	}
+}
+
+func TestGetAppUser(t *testing.T) {
+	writeDb := controller.GetWriteDB()
+	defer writeDb.Close()
+	readDb := controller.GetReadDB()
+	defer readDb.Close()
+
+	var result AppUser
+
+	name := uniuri.New()
+	email := "test@test.com"
+	mibaID := uint(2)
+
+	appuser := AppUser{Name: name, Email: email, MiBAID: mibaID}
+	record := writeDb.Create(&appuser)
+
+	if value, ok := record.Value.(*AppUser); ok {
+		readDb.First(&result, value.ID)
+
+		require.Equal(t, value.Name, result.Name)
+		require.Equal(t, email, result.Email)
+		require.Equal(t, mibaID, result.MiBAID)
+	} else {
+		t.Fatal("Value is not aan AppUser")
+	}
+}
