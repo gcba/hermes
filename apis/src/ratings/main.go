1c557d18fca48659ac9b796c25ec4314b685cdf0
Rita <zeta@widcket.com>
2017-06-13 12:24:17 -0300

    Added new dependency

--- apis/src/ratings/main.go
+++ apis/src/ratings/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/facebookgo/grace/gracehttp"
 	_ "github.com/joho/godotenv/autoload" // Loads config from .env file
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
+
+type Request struct {
+	Rating  int    `json:"rating"`
+	Comment string `json:"comment"`
+}
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.POST("/ratings", createRating)
+
+	e.Server.Addr = ":3000"
+	e.Logger.Fatal(gracehttp.Serve(e.Server))
+}
+
+func createRating(c echo.Context) error {
+
 }
