8acee1dc585532e1856e0e12fddeff6141813053
Rita <zeta@widcket.com>
2017-06-19 15:43:27 -0300

    Code format fix

--- apis/src/ratings/responses/error.go
+++ apis/src/ratings/responses/error.go
@@ -1,11 +1,9 @@
 package responses
 
-type (
-	Error struct {
-		Meta   meta     `json:"meta"`
-		Errors []string `json:"errors"`
-	}
-)
+type Error struct {
+	Meta   meta     `json:"meta"`
+	Errors []string `json:"errors"`
+}
 
 func ErrorResponse(status int, singleError string, context echo.Context) error {
 	return ErrorsResponse(status, []string{singleError}, context)
