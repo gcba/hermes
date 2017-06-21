a7f597848009987c6bd0a498eb795dd8e4ed2b37
Rita <zeta@widcket.com>
2017-06-21 19:18:18 -0300

    Added whitespace validation rule

--- apis/src/ratings/parser/parser.go
+++ apis/src/ratings/parser/parser.go
@@ -11,24 +11,24 @@ import (
 
 type (
 	app struct {
-		Key     string `json:"key" validate:"required,len=32,alphanum"` // The app key
-		Version string `json:"version" validate:"required,alphanum,gte=1,lte=15"`
+		Key     string `json:"key" validate:"required,len=32,alphanum,excludesall= "` // The app key
+		Version string `json:"version" validate:"required,alphanum,gte=1,lte=15,excludesall= "`
 	}
 
 	user struct {
-		Name   string `json:"name" validate:"required,alphanum,gte=3,slte=70"`
-		Email  string `json:"email" validate:"required,email,gte=3,lte=100"`
-		MiBAID string `json:"mibaId" validate:"required,alphanum,gte=1"`
+		Name   string `json:"name" validate:"required,alphanum,gte=3,slte=70,excludesall= "`
+		Email  string `json:"email" validate:"required,email,gte=3,lte=100,excludesall= "`
+		MiBAID string `json:"mibaId" validate:"required,alphanum,gte=1,excludesall= "`
 	}
 
 	platform struct {
-		Key     string `json:"key" validate:"required,len=32,alphanum"` // The platform key
-		Version string `json:"version" validate:"required,alphanum,gte=1,lte=15"`
+		Key     string `json:"key" validate:"required,len=32,alphanum,excludesall= "` // The platform key
+		Version string `json:"version" validate:"required,alphanum,gte=1,lte=15,excludesall= "`
 	}
 
 	device struct {
-		Name   string `json:"name" validate:"required,alphanum,gte=1,lte=30"`
-		Brand  string `json:"brand" validate:"required,alphanum,gte=1,lte=30"`
+		Name   string `json:"name" validate:"required,alphanum,gte=1,lte=30,excludesall= "`
+		Brand  string `json:"brand" validate:"required,alphanum,gte=1,lte=30,excludesall= "`
 		Screen screen `json:"screen" validate:"required"`
 	}
 
@@ -39,16 +39,16 @@ type (
 	}
 
 	browser struct {
-		Name    string `json:"name" validate:"required,alphanum,gte=1,lte=15"`
-		Version string `json:"version" validate:"required,alphanum,gte=1,lte=15"`
+		Name    string `json:"name" validate:"required,alphanum,gte=1,lte=15,excludesall= "`
+		Version string `json:"version" validate:"required,alphanum,gte=1,lte=15,excludesall= "`
 	}
 
 	// Request holds the mapped fields from the request's JSON body
 	Request struct {
 		Rating      uint8    `json:"rating" validate:"required,numeric,min=-127,max=127"`
-		Description string   `json:"description" validate:"alphanum,gte=3,lte=30,omitempty"`
-		Comment     string   `json:"comment" validate:"alphanum,gte=3,lte=1000,omitempty"`
-		Range       string   `json:"range" validate:"required,len=32,alphanum"` // The range key
+		Description string   `json:"description" validate:"alphanum,gte=3,lte=30,omitempty,excludesall= "`
+		Comment     string   `json:"comment" validate:"alphanum,gte=3,lte=1000,omitempty,excludesall= "`
+		Range       string   `json:"range" validate:"required,len=32,alphanum,excludesall= "` // The range key
 		App         app      `json:"app" validate:"required"`
 		Platform    platform `json:"platform" validate:"required"`
 		User        *user    `json:"user" validate:"omitempty"`
