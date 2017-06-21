a47590e37d90a68f9ad2cfc8a020857bfef76b4a
Rita <zeta@widcket.com>
2017-06-21 19:21:01 -0300

    Fixed validations

--- apis/src/ratings/parser/parser.go
+++ apis/src/ratings/parser/parser.go
@@ -16,7 +16,7 @@ type (
 	}
 
 	user struct {
-		Name   string `json:"name" validate:"required,alphanum,gte=3,slte=70,excludesall= "`
+		Name   string `json:"name" validate:"required,alphanum,gte=3,slte=70"`
 		Email  string `json:"email" validate:"required,email,gte=3,lte=100,excludesall= "`
 		MiBAID string `json:"mibaId" validate:"required,alphanum,gte=1,excludesall= "`
 	}
@@ -27,8 +27,8 @@ type (
 	}
 
 	device struct {
-		Name   string `json:"name" validate:"required,alphanum,gte=1,lte=30,excludesall= "`
-		Brand  string `json:"brand" validate:"required,alphanum,gte=1,lte=30,excludesall= "`
+		Name   string `json:"name" validate:"required,alphanum,gte=1,lte=30"`
+		Brand  string `json:"brand" validate:"required,alphanum,gte=1,lte=30"`
 		Screen screen `json:"screen" validate:"required"`
 	}
 
@@ -39,15 +39,15 @@ type (
 	}
 
 	browser struct {
-		Name    string `json:"name" validate:"required,alphanum,gte=1,lte=15,excludesall= "`
+		Name    string `json:"name" validate:"required,alphanum,gte=1,lte=15"`
 		Version string `json:"version" validate:"required,alphanum,gte=1,lte=15,excludesall= "`
 	}
 
 	// Request holds the mapped fields from the request's JSON body
 	Request struct {
 		Rating      uint8    `json:"rating" validate:"required,numeric,min=-127,max=127"`
-		Description string   `json:"description" validate:"alphanum,gte=3,lte=30,omitempty,excludesall= "`
-		Comment     string   `json:"comment" validate:"alphanum,gte=3,lte=1000,omitempty,excludesall= "`
+		Description string   `json:"description" validate:"alphanum,gte=3,lte=30,omitempty"`
+		Comment     string   `json:"comment" validate:"alphanum,gte=3,lte=1000,omitempty"`
 		Range       string   `json:"range" validate:"required,len=32,alphanum,excludesall= "` // The range key
 		App         app      `json:"app" validate:"required"`
 		Platform    platform `json:"platform" validate:"required"`
