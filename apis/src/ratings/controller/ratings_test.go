355876c6611c4572b7a2ef3e921309d0ef56ce55
Rita Zerrizuela <zeta@widcket.com>
2017-06-22 20:22:45 -0300

    Added test for endpoint OPTIONS /ratings

---
+++ apis/src/ratings/controller/ratings_test.go
@@ -0,0 +1,53 @@
+package controller
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"ratings/handler"
+
+	"github.com/gavv/httpexpect"
+	"github.com/labstack/echo"
+)
+
+func TestOptionsRatings(t *testing.T) {
+	routes := map[string]echo.HandlerFunc{
+		"OptionsRoot":    OptionsRoot,
+		"OptionsRatings": OptionsRatings,
+		"PostRatings":    PostRatings}
+
+	handler := handler.Handler(3000, routes)
+	server := httptest.NewServer(handler)
+
+	defer server.Close()
+
+	server.URL = "http://localhost:3000"
+
+	e := httpexpect.WithConfig(httpexpect.Config{
+		BaseURL:  server.URL,
+		Reporter: httpexpect.NewAssertReporter(t),
+		Printers: []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, true),
+		},
+	})
+
+	json := map[string]interface{}{
+		"meta": map[string]interface{}{
+			"code":    200,
+			"message": "OK"},
+		"endpoints": []map[string]interface{}{
+			{
+				"method": "POST",
+				"path":   "/ratings",
+				"headers": [][]string{
+					{
+						"Content-Type",
+						"application/json; charset=utf-8"}}}}}
+
+	e.OPTIONS("/ratings").WithJSON(json).
+		WithHeader("Content-Type", "application/json; charset=utf-8").
+		WithHeader("Allow", "OPTIONS POST").
+		Expect().
+		Status(http.StatusOK)
+}
