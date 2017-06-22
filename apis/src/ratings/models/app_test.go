05567e34df601c0f288419af5071c398f47ff85e
Rita Zerrizuela <zeta@widcket.com>
2017-06-22 11:30:48 -0300

    Added tests

--- apis/src/ratings/models/app_test.go
+++ apis/src/ratings/models/app_test.go
@@ -1,5 +1,36 @@
 package models
 
 import (
+	"ratings/database"
+	"strings"
+	"testing"
+
+	"github.com/dchest/uniuri"
 	_ "github.com/joho/godotenv/autoload" // Loads config from .env file
+	"github.com/stretchr/testify/require"
 )
+
+func TestGetApp(t *testing.T) {
+	writeDb := database.GetWriteDB()
+	defer writeDb.Close()
+	readDb := database.GetReadDB()
+	defer readDb.Close()
+
+	name := uniuri.New()
+	key := "7C6F0035B18C3D5J" + strings.ToUpper(uniuri.New())
+
+	app := App{Name: name, Key: key}
+	result := writeDb.Create(&app)
+
+	require.Equal(t, nil, result.Error)
+
+	if value, ok := result.Value.(*App); ok {
+		var result App
+
+		readDb.First(&result, value.ID)
+		require.Equal(t, name, value.Name)
+		require.Equal(t, key, value.Key)
+	} else {
+		t.Fatal("Value is not an App")
+	}
+}
--- apis/src/ratings/models/range_test.go
+++ apis/src/ratings/models/range_test.go
@@ -1,5 +1,38 @@
 package models
 
 import (
+	"ratings/database"
+	"strings"
+	"testing"
+
+	"github.com/dchest/uniuri"
 	_ "github.com/joho/godotenv/autoload" // Loads config from .env file
+	"github.com/stretchr/testify/require"
 )
+
+func TestGetRange(t *testing.T) {
+	writeDb := database.GetWriteDB()
+	defer writeDb.Close()
+	readDb := database.GetReadDB()
+	defer readDb.Close()
+
+	from := 0
+	to := 5
+	key := "7C6F0035B18C3D5J" + strings.ToUpper(uniuri.New())
+
+	rangeRecord := Range{From: 0, To: 5, Key: key}
+	result := writeDb.Create(&rangeRecord)
+
+	require.Equal(t, nil, result.Error)
+
+	if value, ok := result.Value.(*Range); ok {
+		var result Range
+
+		readDb.First(&result, value.ID)
+		require.Equal(t, from, value.From)
+		require.Equal(t, to, value.To)
+		require.Equal(t, key, value.Key)
+	} else {
+		t.Fatal("Value is not a Range")
+	}
+}
