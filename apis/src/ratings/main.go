3103f3e9eabbbf891d167e3332fabe11b6daed5b
Rita Zerrizuela <zeta@widcket.com>
2017-06-22 20:01:41 -0300

    Extracted handler to its own package

--- apis/src/ratings/main.go
+++ apis/src/ratings/main.go
@@ -5,43 +5,21 @@ import (
 	"strconv"
 
 	"ratings/controller"
+	"ratings/handler"
 
 	"github.com/facebookgo/grace/gracehttp"
-	"github.com/go-playground/validator"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
-	"net/http"
 )
-
-type RequestValidator struct {
-	validator *validator.Validate
-}
-
-func (rv *RequestValidator) Validate(request interface{}) error {
-	return rv.validator.Struct(request)
-}
-
-func Handler(port int) http.Handler {
-	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Use(middleware.Secure())
-	e.Use(middleware.BodyLimit("10K"))
-
-	e.Server.Addr = ":" + strconv.Itoa(port)
-	e.Validator = &RequestValidator{validator: validator.New()} // TODO: Move this to parser.AttachValidator()
-
-	e.OPTIONS("/", controller.OptionsRoot)
-	e.OPTIONS("/ratings", controller.OptionsRatings)
-	e.POST("/ratings", controller.PostRatings)
-
-	return e
-}
 
 func main() {
 	port := 3000
-	handler := Handler(port).(*echo.Echo) // Casting via type assertion
+
+	routes := map[string]echo.HandlerFunc{
+		"OptionsRoot":   controller.OptionsRoot,
+		"OptionsRating": controller.OptionsRatings,
+		"PostRatings":   controller.PostRatings}
+
+	handler := handler.Handler(port, routes).(*echo.Echo) // Casting via type assertion
 
 	fmt.Println("Started server on port", strconv.Itoa(port))
 
