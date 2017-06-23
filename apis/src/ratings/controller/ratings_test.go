5ac482da09952f704bf5780646d84c262e013bc6
Rita Zerrizuela <zeta@widcket.com>
2017-06-22 23:34:05 -0300

    Added test for endpoint POST /ratings

--- apis/src/ratings/controller/ratings_test.go
+++ apis/src/ratings/controller/ratings_test.go
@@ -11,12 +11,12 @@ import (
 	"github.com/labstack/echo"
 )
 
-func TestOptionsRatings(t *testing.T) {
-	routes := map[string]echo.HandlerFunc{
-		"OptionsRoot":    OptionsRoot,
-		"OptionsRatings": OptionsRatings,
-		"PostRatings":    PostRatings}
+var routes = map[string]echo.HandlerFunc{
+	"OptionsRoot":    OptionsRoot,
+	"OptionsRatings": OptionsRatings,
+	"PostRatings":    PostRatings}
 
+func TestOptionsRatings(t *testing.T) {
 	handler := handler.Handler(3000, routes)
 	server := httptest.NewServer(handler)
 
@@ -50,4 +50,44 @@ func TestOptionsRatings(t *testing.T) {
 		WithHeader("Allow", "OPTIONS POST").
 		Expect().
 		Status(http.StatusOK)
+}
+
+func TestPostRatings(t *testing.T) {
+	handler := handler.Handler(3000, routes)
+	server := httptest.NewServer(handler)
+
+	defer server.Close()
+
+	server.URL = "http://localhost:3000"
+
+	e := httpexpect.WithConfig(httpexpect.Config{
+		BaseURL:  server.URL,
+		Reporter: httpexpect.NewAssertReporter(t),
+		Printers: []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, true),
+		},
+	})
+
+	jsonRequest := map[string]interface{}{
+		"rating":      uint8(3),
+		"description": "Regular",
+		"range":       "e10adc3949ba59abbe56e057f20f883e",
+		"app": map[string]interface{}{
+			"key":     "e10adc3949ba59abbe56e057f20f883e",
+			"version": "2.O"},
+		"platform": map[string]interface{}{
+			"key":     "e10adc3949ba59abbe56e057f20f883e",
+			"version": "9.O"}}
+
+	/*
+		jsonResponse := map[string]interface{}{
+			"meta": map[string]interface{}{
+				"code":    201,
+				"message": "Created"}}}
+	*/
+
+	e.POST("/ratings").WithJSON(jsonRequest).
+		WithHeader("Content-Type", "application/json; charset=utf-8").
+		Expect().
+		Status(http.StatusCreated)
 }
