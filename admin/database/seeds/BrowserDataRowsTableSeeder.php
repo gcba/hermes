bcabcea1ac9c41e475b2fa9d1abf57bbfb5ce1f0
Rita <zeta@widcket.com>
2017-07-03 09:51:41 -0300

    Added validation rules for Browser

--- admin/database/seeds/BrowserDataRowsTableSeeder.php
+++ admin/database/seeds/BrowserDataRowsTableSeeder.php
@@ -40,7 +40,7 @@ class BrowserDataRowsTableSeeder extends Seeder
                 'edit'         => 0,
                 'add'          => 0,
                 'delete'       => 0,
-                'details'      => '',
+                'details'      => '{"validation":{"rules":["required","string","min:1","max:30"],"messages":{"required":"Falta el campo :attribute.","string":"El campo :attribute debe tener texto.","max":"El campo :attribute puede tener hasta :max carácteres.","min":"El campo :attribute debe tener al menos :min carácteres."}}}',
                 'order'        => 2,
             ])->save();
         }
