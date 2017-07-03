5ed6806169f073d20e4c5240c79530ee87345614
Rita <zeta@widcket.com>
2017-07-03 14:51:09 -0300

    Fixed seeds bug

--- admin/database/seeds/DataTypesTableSeeder.php
+++ admin/database/seeds/DataTypesTableSeeder.php
@@ -16,11 +16,12 @@ class DataTypesTableSeeder extends Seeder
                 'name'                  => 'users',
                 'display_name_singular' => 'User',
                 'display_name_plural'   => 'Users',
+                'slug'                  => 'users',
                 'icon'                  => 'voyager-person',
                 'model_name'            => 'TCG\\Voyager\\Models\\User',
                 'controller'            => 'Controller',
-                'generate_permissions'  => 1,
-                'description'           => '',
+                'generate_permissions'  => 0,
+                'description'           => 'Personal',
             ])->save();
         }
 
@@ -30,11 +31,12 @@ class DataTypesTableSeeder extends Seeder
                 'name'                  => 'apps',
                 'display_name_singular' => 'App',
                 'display_name_plural'   => 'Apps',
+                'slug'                  => 'apps',
                 'icon'                  => 'voyager-categories',
                 'model_name'            => 'App\\App',
                 'controller'            => 'Controller',
-                'generate_permissions'  => 1,
-                'description'           => 'App',
+                'generate_permissions'  => 0,
+                'description'           => 'Aplicaciones',
             ])->save();
         }
 
@@ -44,10 +46,11 @@ class DataTypesTableSeeder extends Seeder
                 'name'                  => 'appusers',
                 'display_name_singular' => 'App User',
                 'display_name_plural'   => 'App Users',
+                'slug'                  => 'appusers',
                 'icon'                  => 'voyager-people',
                 'model_name'            => 'App\\AppUser',
                 'controller'            => 'Controller',
-                'generate_permissions'  => 1,
+                'generate_permissions'  => 0,
                 'description'           => 'Usuarios de las aplicaciones',
             ])->save();
         }
@@ -55,13 +58,14 @@ class DataTypesTableSeeder extends Seeder
         $dataType = $this->dataType('slug', 'brands');
         if (!$dataType->exists) {
             $dataType->fill([
-                'name'                  => 'brand',
+                'name'                  => 'brands',
                 'display_name_singular' => 'Brand',
                 'display_name_plural'   => 'Brands',
+                'slug'                  => 'brands',
                 'icon'                  => 'voyager-tag',
                 'model_name'            => 'App\\Brand',
                 'controller'            => 'Controller',
-                'generate_permissions'  => 1,
+                'generate_permissions'  => 0,
                 'description'           => 'Marcas de los dispositivos',
             ])->save();
         }
@@ -69,13 +73,14 @@ class DataTypesTableSeeder extends Seeder
         $dataType = $this->dataType('slug', 'browsers');
         if (!$dataType->exists) {
             $dataType->fill([
-                'name'                  => 'browser',
+                'name'                  => 'browsers',
                 'display_name_singular' => 'Browser',
                 'display_name_plural'   => 'Browsers',
+                'slug'                  => 'browsers',
                 'icon'                  => 'voyager-browser',
                 'model_name'            => 'App\\Browser',
                 'controller'            => 'Controller',
-                'generate_permissions'  => 1,
+                'generate_permissions'  => 0,
                 'description'           => 'Navegadores',
             ])->save();
         }
@@ -83,13 +88,14 @@ class DataTypesTableSeeder extends Seeder
         $dataType = $this->dataType('slug', 'devices');
         if (!$dataType->exists) {
             $dataType->fill([
-                'name'                  => 'device',
+                'name'                  => 'devices',
                 'display_name_singular' => 'Device',
                 'display_name_plural'   => 'Devices',
+                'slug'                  => 'devices',
                 'icon'                  => 'voyager-phone',
                 'model_name'            => 'App\\Device',
                 'controller'            => 'Controller',
-                'generate_permissions'  => 1,
+                'generate_permissions'  => 0,
                 'description'           => 'Dispositivos móviles',
             ])->save();
         }
@@ -97,13 +103,14 @@ class DataTypesTableSeeder extends Seeder
         $dataType = $this->dataType('slug', 'messages');
         if (!$dataType->exists) {
             $dataType->fill([
-                'name'                  => 'message',
+                'name'                  => 'messages',
                 'display_name_singular' => 'Message',
                 'display_name_plural'   => 'Messages',
+                'slug'                  => 'messages',
                 'icon'                  => 'voyager-chat',
                 'model_name'            => 'App\\Message',
                 'controller'            => 'Controller',
-                'generate_permissions'  => 1,
+                'generate_permissions'  => 0,
                 'description'           => 'Mensajes',
             ])->save();
         }
@@ -111,13 +118,14 @@ class DataTypesTableSeeder extends Seeder
         $dataType = $this->dataType('slug', 'platforms');
         if (!$dataType->exists) {
             $dataType->fill([
-                'name'                  => 'platform',
+                'name'                  => 'platforms',
                 'display_name_singular' => 'Platform',
                 'display_name_plural'   => 'Platforms',
+                'slug'                  => 'platforms',
                 'icon'                  => 'voyager-laptop',
                 'model_name'            => 'App\\Platform',
                 'controller'            => 'Controller',
-                'generate_permissions'  => 1,
+                'generate_permissions'  => 0,
                 'description'           => 'Plataformas donde andan las aplicaciones',
             ])->save();
         }
@@ -125,13 +133,14 @@ class DataTypesTableSeeder extends Seeder
         $dataType = $this->dataType('slug', 'ranges');
         if (!$dataType->exists) {
             $dataType->fill([
-                'name'                  => 'range',
+                'name'                  => 'ranges',
                 'display_name_singular' => 'Range',
                 'display_name_plural'   => 'Ranges',
+                'slug'                  => 'ranges',
                 'icon'                  => 'voyager-star-half',
                 'model_name'            => 'App\\Range',
                 'controller'            => 'Controller',
-                'generate_permissions'  => 1,
+                'generate_permissions'  => 0,
                 'description'           => 'Rangos de calificaciones',
             ])->save();
         }
@@ -139,13 +148,14 @@ class DataTypesTableSeeder extends Seeder
         $dataType = $this->dataType('slug', 'ratings');
         if (!$dataType->exists) {
             $dataType->fill([
-                'name'                  => 'rating',
+                'name'                  => 'ratings',
                 'display_name_singular' => 'Rating',
                 'display_name_plural'   => 'Ratings',
+                'slug'                  => 'ratings',
                 'icon'                  => 'voyager-star-two',
                 'model_name'            => 'App\\Rating',
                 'controller'            => 'Controller',
-                'generate_permissions'  => 1,
+                'generate_permissions'  => 0,
                 'description'           => 'Calificaciones',
             ])->save();
         }
@@ -156,11 +166,12 @@ class DataTypesTableSeeder extends Seeder
                 'name'                  => 'menus',
                 'display_name_singular' => 'Menu',
                 'display_name_plural'   => 'Menus',
+                'slug'                  => 'menus',
                 'icon'                  => 'voyager-list',
                 'model_name'            => 'TCG\\Voyager\\Models\\Menu',
                 'controller'            => 'Controller',
-                'generate_permissions'  => 1,
-                'description'           => '',
+                'generate_permissions'  => 0,
+                'description'           => 'Menús',
             ])->save();
         }
 
@@ -170,11 +181,12 @@ class DataTypesTableSeeder extends Seeder
                 'name'                  => 'roles',
                 'display_name_singular' => 'Role',
                 'display_name_plural'   => 'Roles',
+                'slug'                  => 'roles',
                 'icon'                  => 'voyager-lock',
                 'model_name'            => 'TCG\\Voyager\\Models\\Role',
                 'controller'            => 'Controller',
-                'generate_permissions'  => 1,
-                'description'           => '',
+                'generate_permissions'  => 0,
+                'description'           => 'Roles',
             ])->save();
         }
     }
