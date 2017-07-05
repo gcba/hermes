1d32da3ccf0bca28a5a352d4b1782fef5ef8b54b
Rita <zeta@widcket.com>
2017-07-05 14:53:22 -0300

    Added accessors for created_at and updated_at in Browser

--- admin/app/Browser.php
+++ admin/app/Browser.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use DateTime;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -26,9 +27,36 @@ class Browser extends Model
     protected $dates = ['deleted_at'];
 
     /**
+     * Boot function for using with User Events
+     *
+     * @return void
+     */
+    protected static function boot() {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->attributes['updated_at'] = null;
+        });
+    }
+
+    /**
      * Get the ratings that belong to this browser.
      */
     public function ratings() {
         return $this->hasMany('App\Rating', 'rating_id', 'id');
+    }
+
+    public function getCreatedAtAttribute(){
+        return $this->formatDate($this->attributes['created_at']);
+    }
+
+    public function getUpdatedAtAttribute(){
+        return $this->attributes['updated_at'] ? $this->formatDate($this->attributes['updated_at']) : '-';
+    }
+
+    private function formatDate($dateString) {
+        $date = new DateTime($dateString);
+
+        return $date->format('d/m/Y H:i:s');
     }
 }
