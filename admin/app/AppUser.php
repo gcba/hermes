74c15508d9f41abbaf667ea8cb2bf591c2d13663
Rita <zeta@widcket.com>
2017-07-05 14:53:11 -0300

    Added accessors for created_at and updated_at in AppUser

--- admin/app/AppUser.php
+++ admin/app/AppUser.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use DateTime;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -26,6 +27,19 @@ class AppUser extends Model
      * @var array
      */
     protected $dates = ['deleted_at'];
+
+    /**
+     * Boot function for using with User Events
+     *
+     * @return void
+     */
+    protected static function boot() {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->attributes['updated_at'] = null;
+        });
+    }
 
     /**
      * Get the ratings that belong to this app user.
@@ -54,4 +68,18 @@ class AppUser extends Model
      public function devices() {
         return $this->belongsToMany('App\Device');
      }
+
+     public function getCreatedAtAttribute(){
+        return $this->formatDate($this->attributes['created_at']);
+    }
+
+    public function getUpdatedAtAttribute(){
+        return $this->attributes['updated_at'] ? $this->formatDate($this->attributes['updated_at']) : '-';
+    }
+
+    private function formatDate($dateString) {
+        $date = new DateTime($dateString);
+
+        return $date->format('d/m/Y H:i:s');
+    }
 }
