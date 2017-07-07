aeba315d06cd9066995f35ddd5ad761bf752c0de
Rita <zeta@widcket.com>
2017-07-07 20:07:39 -0300

    Added NotImplemented error test

--- apis/src/ratings/controller/root_test.go
+++ apis/src/ratings/controller/root_test.go
@@ -122,3 +122,33 @@ func TestOptions_NotFoundError(t *testing.T) {
 	r.Header("Content-Type").Equal("application/json; charset=UTF-8")
 	r.JSON().Object().Equal(response)
 }
+
+func TestOptions_NotImplementedError(t *testing.T) {
+	handler := handler.Handler(3000, routes)
+	server := httptest.NewServer(handler)
+
+	defer server.Close()
+
+	server.URL = "http://localhost:3000"
+
+	e := httpexpect.WithConfig(httpexpect.Config{
+		BaseURL:  server.URL,
+		Reporter: httpexpect.NewAssertReporter(t),
+		Printers: []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, true),
+		},
+	})
+
+	response := map[string]interface{}{
+		"meta": map[string]interface{}{
+			"code":    http.StatusNotImplemented,
+			"message": "Not Implemented"}}
+
+	r := e.GET("/").
+		WithHeader("Accept", "application/json").
+		Expect()
+
+	r.Status(http.StatusNotImplemented)
+	r.Header("Content-Type").Equal("application/json; charset=UTF-8")
+	r.JSON().Object().Equal(response)
+}
