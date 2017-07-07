e24920601da6fc709555fd252e113852d92eeaaf
Rita <zeta@widcket.com>
2017-07-07 16:37:21 -0300

    Updated conenction logic to work with latest driver

--- apis/src/ratings/database/connection.go
+++ apis/src/ratings/database/connection.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres" // postgres driver
-	_ "github.com/jinzhu/gorm/dialects/sqlite"   // sqlite driver
 	"github.com/joho/godotenv"
 )
 
@@ -23,60 +22,50 @@ var (
 	env = os.Getenv("API_RATINGS_ENV")
 
 	// Load read database settings
-	readDBName     = os.Getenv("API_RATINGS_READDB_NAME")
+	readDBPort     = os.Getenv("API_RATINGS_READDB_PORT")
 	readDBHost     = os.Getenv("API_RATINGS_READDB_HOST")
+	readDBName     = os.Getenv("API_RATINGS_READDB_NAME")
 	readDBUser     = os.Getenv("API_RATINGS_READDB_USER")
 	readDBPassword = os.Getenv("API_RATINGS_READDB_PASSWORD")
 
 	// Load write database settings
-	writeDBName     = os.Getenv("API_RATINGS_WRITEDB_NAME")
+	writeDBPort     = os.Getenv("API_RATINGS_WRITEDB_PORT")
 	writeDBHost     = os.Getenv("API_RATINGS_WRITEDB_HOST")
+	writeDBName     = os.Getenv("API_RATINGS_WRITEDB_NAME")
 	writeDBUser     = os.Getenv("API_RATINGS_WRITEDB_USER")
 	writeDBPassword = os.Getenv("API_RATINGS_WRITEDB_PASSWORD")
 )
 
 // GetReadDB connects to the read database and returns a pointer to the connection
 func GetReadDB() *gorm.DB {
-	if env != "PRODUCTION" {
-		credentials := fmt.Sprintf(
-			"host=%s user=%s dbname=%s sslmode=disable password=%s", // TODO: Handle sslmode
-			readDBHost,
-			readDBUser,
-			readDBName,
-			readDBPassword)
-
-		return connectDB("postgres", credentials)
-	}
-
-	credentials := fmt.Sprintf(
-		"host=%s user=%s dbname=%s sslmode=disable password=%s", // TODO: Handle sslmode
-		readDBHost,
-		readDBUser,
-		readDBName,
-		readDBPassword)
-
-	return connectDB("postgres", credentials)
+	return getDB(readDBHost, readDBPort, readDBName, readDBUser, readDBPassword)
 }
 
 // GetWriteDB connects to the write database and returns a pointer to the connection
 func GetWriteDB() *gorm.DB {
+	return getDB(writeDBHost, writeDBPort, writeDBName, writeDBUser, writeDBPassword)
+}
+
+func getDB(host string, port string, db string, user string, password string) *gorm.DB {
 	if env != "PRODUCTION" {
 		credentials := fmt.Sprintf(
-			"host=%s user=%s dbname=%s sslmode=disable password=%s", // TODO: Handle sslmode
-			readDBHost,
-			readDBUser,
-			readDBName,
-			readDBPassword)
+			"host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", // TODO: Handle sslmode
+			host,
+			port,
+			user,
+			db,
+			password)
 
 		return connectDB("postgres", credentials)
 	}
 
 	credentials := fmt.Sprintf(
-		"host=%s user=%s dbname=%s sslmode=disable password=%s", // TODO: Handle sslmode
-		writeDBHost,
-		writeDBUser,
-		writeDBName,
-		writeDBPassword)
+		"host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", // TODO: Handle sslmode
+		host,
+		port,
+		user,
+		db,
+		password)
 
 	return connectDB("postgres", credentials)
 }
