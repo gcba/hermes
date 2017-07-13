ce1331f502c320582cad4e7439ac7ff0e3bff429
Rita <zeta@widcket.com>
2017-07-13 18:40:49 -0300

    Latest build

--- sdks/web/dist/ratings.js
+++ sdks/web/dist/ratings.js
@@ -2930,43 +2930,93 @@ if (!window.Promise) {
     window.Promise = promise;
 }
 
-const isString = thing => {
-    return typeof thing === 'string' || thing instanceof String;
+const fail = message => {
+    throw new Error({ name: 'RatingError', message: message });
 };
 
-const isBool = thing => {
-    return typeof thing === "boolean";
+const check = {
+    isString: thing => {
+        return typeof thing === 'string' || thing instanceof String;
+    },
+    isBool: thing => {
+        return typeof thing === 'boolean';
+    },
+    isInteger: thing => {
+        return typeof val === 'number' && isFinite(val) && Math.floor(val) === val;
+    },
+    isPlainObject: thing => {
+        return typeof thing === 'object' && thing !== null && thing.constructor === Object && thing.hasOwnProperty('isPrototypeOf') === false && thing.toString() === '[object Object]';
+    }
 };
 
-const isInteger = thing => {
-    return typeof val === "number" && isFinite(val) && Math.floor(val) === val;
-};
+const validate = {
+    rating: value => {
+        if (value && check.isInteger(value) && value >= -127 && value <= 127) return value;
 
-const errorNamespace = 'RatingError';
+        fail('Invalid or missing rating');
+    },
+    description: value => {
+        if (check.isString(value) && value.trim().length >= 3 && value.trim().length <= 30) return value.trim();
+
+        fail('Invalid description');
+    },
+    comment: value => {
+        if (check.isString(value) && value.trim().length >= 3 && value.trim().length <= 1000) return value.trim();
+
+        fail('Invalid comment');
+    },
+    key: (value, name) => {
+        if (value && check.isString(value.trim()) && value.trim().length === 32) return value.trim();
+
+        fail('Invalid or missing ' + name);
+    },
+    token: value => {
+        if (value && check.isString(value) && value.trim().length > 0) return value.trim();
+
+        fail('Invalid or missing token');
+    },
+    url: value => {
+        const url = new RegExp(/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i);
+
+        if (value && check.isString(value) && value.trim().length > 0 && url.test(value.trim())) return value.trim();
+
+        fail('Invalid or missing api');
+    },
+    appVersion: value => {
+        if (value && check.isString(value) && value.trim().length >= 1 && value.trim().length <= 15) return value.trim();
+
+        fail('Invalid or missing version');
+    },
+    isMobile: value => {
+        if (value === undefined || value === null || check.isBool(value)) return value;
+
+        fail('Invalid isMobile');
+    },
+    userAgent: value => {
+        if (check.isString(value) && value.trim().length > 0) return value.trim();
+
+        fail('Invalid userAgent');
+    }
+};
 
 class Complaint {
     constructor(options) {
-        // Should fail when:
-        // - Required things are missing
-        // - Things are invalid: format, type
+        this._keys = {};
+        this._versions = {};
+        this._screen = {};
 
-        this.keys = {}; // TODO: Make private modifying the descriptor
-        this.versions = {}; // TODO: Make private modifying the descriptor
-        this.screen = {}; // TODO: Make private modifying the descriptor
+        this._keys.app = validate.key(options.app, 'app');
+        this._keys.platform = validate.key(options.platform, 'platform');
+        this._keys.range = validate.key(options.range, 'range');
 
-        this.keys.app = this.validateKey(options.app);
-        this.keys.platform = this.validateKey(options.platform);
-        this.keys.range = this.validateKey(options.range);
+        this.appVersion = validate.appVersion(options.version);
+        this._url = validate.url(options.api);
+        this._token = validate.token(options.token);
+        this._userAgent = options.userAgent ? validate.userAgent(options.userAgent) : window.navigator.userAgent;
+        this._isMobile = validate.isMobile(options.isMobile);
 
-        this.versions.app = options.appVersion; // TODO: Validate (maybe in a proxy?) Required
-
-        this.url = options.api; // TODO: Required
-        this.token = options.token; // TODO: Required
-        this._userAgent = options.userAgent; // TODO: Make private
-        this._isMobile = options.isMobile; // TODO: Make private
-
-        this.mobileDetect = new mobileDetect$1(this.userAgent || window.navigator.userAgent);
-        this.platform = platform.parse(this.userAgent || window.navigator.userAgent);
+        this._mobileDetect = new mobileDetect$1(this._userAgent);
+        this._platform = platform.parse(this._userAgent);
     }
 
     get isMobile() {
@@ -2975,32 +3025,30 @@ class Complaint {
 
     get app() {
         return {
-            key: this.keys.app,
-            version: this.versions.app
+            key: this._keys.app,
+            version: this._appVersion
         };
     }
 
     get platform() {
         return {
-            key: this.keys.platform,
-            version: platform.os.split(' ').pop()
+            key: this._keys.platform,
+            version: this._platform.os.split(' ').pop()
         };
     }
 
     get device() {
         const result = {
-            name: this.isMobile ? platform.product : 'Desktop',
+            name: this.isMobile ? this._platform.product : 'Desktop',
             screen: this.screen
         };
 
-        if (this.isMobile && platform.manufacturer) result.brand = platform.manufacturer;
+        if (this.isMobile && this._platform.manufacturer) result.brand = this._platform.manufacturer;
 
         return result;
     }
 
     get screen() {
-        const mobile = mobileDetect.mobile();
-
         return {
             width: self.screen.width || window.screen.width,
             height: self.screen.height || window.screen.height
@@ -3010,64 +3058,54 @@ class Complaint {
     get user() {
         const result = {};
 
-        if (this.user.name) result.name = this.user.name;
-        if (this.user.email) result.email = this.result.email;
-        if (this.user.mibaId) result.mibaId = this.user.mibaId;
-
-        if (!result.email && !result.mibaId) throw new Error({
-            name: errorNamespace,
-            message: 'User has no email or mibaId'
-        });
+        if (this_.user.name) result.name = this._user.name;
+        if (this_.user.email) result.email = this._user.email;
+        if (this_.user.mibaId) result.mibaId = this._user.mibaId;
 
         return result;
     }
 
     get browser() {
         return {
-            name: platform.name,
-            version: platform.version
+            name: this._platform.name,
+            version: this._platform.version
         };
     }
 
-    set url(value) {
-        const urlRegex = new RegExp(/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i);
-
-        if (isString(value) && urlRegex.test(trim(value))) this.url = value;else throw new Error({ name: errorNamespace, message: 'Invalid URL' });
-    }
-
-    set token(value) {
-        if (isString(value)) this.token = trim(value);else throw new Error({ name: errorNamespace, message: 'Invalid token' });
-    }
-
-    set _isMobile(value) {
-        if (isBool(value)) this._isMobile = value;else throw new Error({ name: errorNamespace, message: 'Invalid isMobile' });
-    }
-
-    set _userAgent(value) {
-        if (isString(value)) this._userAgent = trim(value);else throw new Error({ name: errorNamespace, message: 'Invalid userAgent' });
-    }
-
     set user(value) {
-        // TODO: Validate keys / consider converting into proxy
-        this.user = value;
+        const email = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+
+        const isPlainObject = check.isPlainObject(value);
+        const hasName = check.isString(value.name) && value.name.trim().length > 0;
+        const hasEmail = check.isString(value.email) && value.email.trim().length > 0;
+        const hasMibaId = check.isString(value.mibaId) && value.mibaId.trim().length > 0;
+        const nameIsValid = value.name.trim().length >= 3 && value.name.trim().length <= 70;
+        const emailIsValid = email.test(value.trim()) && value.email.trim().length >= 3 && value.email.trim().length <= 100;
+        const mibaIdIsValid = value.mibaId.trim().length >= 1;
+
+        if (!(isPlainObject && (hasName || hasEmail || hasMibaId))) fail('User object is invalid');
+        if (!(hasEmail && emailIsValid || hasMibaId && mibaIdIsValid)) fail('User has no valid email or mibaId');else this._user = value;
     }
 
     set screen(value) {
-        // TODO: Validate keys / consider converting into proxy
-        this.screen = value;
+        const isPlainObject = check.isPlainObject(value);
+        const hasValidWidth = check.isInteger(value.width) && value > 0;
+        const hasValidHeight = check.isInteger(value.width) && value > 0;
+
+        if (!(isPlainObject && hasValidWidth && hasValidHeight)) fail('Screen object is invalid');else this._screen = value;
     }
 
     create(data) {
         const complaint = {
-            rating: this.validateRating(data.rating),
-            description: this.validateDescription(data.description),
-            comment: this.validateComment(data.comment),
-            range: this.keys.range,
+            rating: validate.rating(data.rating),
+            range: this._keys.range,
             app: this.app,
             platform: this.platform,
             device: this.device
         };
 
+        if (data.description) data.description = validate.description(data.description);
+        if (data.comment) data.comment = validate.comment(data.comment);
         if (this.user) complaint.user = this.user;
         if (this.browser) complaint.browser = this.browser;
 
@@ -3075,7 +3113,6 @@ class Complaint {
     }
 
     send(complaint) {
-        // TODO: make private
         const options = {
             method: 'POST',
             headers: {
@@ -3088,22 +3125,6 @@ class Complaint {
         };
 
         return fetch(this.url, options);
-    }
-
-    validateKey(key) {
-        if (key.length === 32) return key;else throw new Error({ name: errorNamespace, message: 'Invalid key' });
-    }
-
-    validateRating(value) {
-        if (isInteger(value) && value >= -127 && value <= 127) return value;else throw new Error({ name: errorNamespace, message: 'Invalid rating' });
-    }
-
-    validateDescription(value) {
-        if (isString(value) && trim(value).length >= 3 && trim(value).length <= 30) return value;else throw new Error({ name: errorNamespace, message: 'Invalid description' });
-    }
-
-    validateComment(value) {
-        if (isString(value) && trim(value).length >= 3 && trim(value).length <= 1000) return value;else throw new Error({ name: errorNamespace, message: 'Invalid comment' });
     }
 }
 
