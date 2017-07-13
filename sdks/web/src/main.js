258181d2c05899970e5c746d2c328480fffa852f
Rita <zeta@widcket.com>
2017-07-13 14:42:40 -0300

    Added user agent validation

--- sdks/web/src/main.js
+++ sdks/web/src/main.js
@@ -32,10 +32,11 @@ class Complaint {
 
         this.url = options.api; // TODO: Validate (maybe in a proxy?) Required
         this.token = options.token; // TODO: Validate (maybe in a proxy?) Required
+        this._userAgent = options.userAgent; // TODO: Make private
         this._isMobile = options.isMobile; // TODO: Validate (maybe in a proxy?) Make private
 
-        this.mobileDetect = new MobileDetect(options.userAgent || window.navigator.userAgent); // TODO: Validate
-        this.platform = platform.parse(options.userAgent || window.navigator.userAgent); // TODO: Validate
+        this.mobileDetect = new MobileDetect(this.userAgent || window.navigator.userAgent); // TODO: Validate
+        this.platform = platform.parse(this.userAgent || window.navigator.userAgent); // TODO: Validate
     }
 
     get isMobile() {
@@ -100,15 +101,19 @@ class Complaint {
         };
     }
 
-    set token(value) {
-        if (isString(value)) this.token = trim(value);
-    }
-
     set url(value) {
         const urlRegex = new RegExp(/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i);
 
         if (isString(value) && urlRegex.test(trim(value))) this.url = value;
         else throw new Error({ name: 'RatingError', message: 'Invalid URL' });
+    }
+
+    set token(value) {
+        if (isString(value)) this.token = trim(value);
+    }
+
+    set _userAgent(value) {
+        if (isString(value)) this._userAgent = trim(value);
     }
 
     set user(value) { // TODO: Validate keys / consider converting into proxy
