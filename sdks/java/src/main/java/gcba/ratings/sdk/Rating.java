2bb5413dc0199a3a80329780c82a10eb7786c617
Rita <zeta@widcket.com>
2017-07-17 11:38:08 -0300

    Namespaced the package

--- sdks/java/src/main/java/gcba/ratings/sdk/Rating.java
+++ sdks/java/src/main/java/gcba/ratings/sdk/Rating.java
@@ -1,6 +1,6 @@
-package ratings.sdk;
+package gcba.ratings.sdk;
 
-public class SDK {
+public class Rating {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
---
+++ sdks/java/src/main/java/gcba/ratings/sdk/Request.java
@@ -0,0 +1,11 @@
+package gcba.ratings.sdk;
+
+/*
+ * This class represents the JSON payload
+ *
+ */
+class Request {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
