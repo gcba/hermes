1f356f05f63345994812a3ec9d893626b30187f4
Rita <zeta@widcket.com>
2017-07-17 14:49:29 -0300

    Latest build

---
+++ sdks/java/example/sdk/src/main/java/ratings/gcba/sdk/Rating.java
@@ -0,0 +1,11 @@
+package ratings.gcba.sdk;
+
+/**
+ * Created by ritazerrizuela on 7/17/17.
+ */
+
+public final class Rating {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
