76272baf5fbc3a093f17bde6ef989beb30a2940f
Rita Zerrizuela <zeta@widcket.com>
2017-07-21 11:12:29 -0300

    Wired up example form

--- sdks/js/example/js/main.js
+++ sdks/js/example/js/main.js
@@ -1,22 +1,41 @@
-$(document).ready(function() {
-    const sdk = new Ratings({
-        api: 'http://127.0.0.1:5000',
-        token: 'e10adc3949ba59abbe56e057f20f883e',
-        app: 'e10adc3949ba59abbe56e057f20f883e',
-        version: '3.0',
-        range: 'e10adc3949ba59abbe56e057f20f883e',
-        platform: 'e10adc3949ba59abbe56e057f20f883e'
-    });
+$('#result').hide();
 
-    sdk.create({
-        rating: 5,
-        description: 'Regular',
-        comment: 'Lorem ipsum'
-    })
-    .then(() => {
-        console.info('SUCCESS');
-    })
-    .catch((error) => {
-        console.error(error);
+$(document).ready(function () {
+    $("#form").submit(function (e) {
+        e.preventDefault();
+
+        const sdk = new Ratings({
+            api: 'http://127.0.0.1:5000',
+            token: 'e10adc3949ba59abbe56e057f20f883e',
+            app: 'e10adc3949ba59abbe56e057f20f883e',
+            version: '3.0',
+            range: 'e10adc3949ba59abbe56e057f20f883e',
+            platform: 'e10adc3949ba59abbe56e057f20f883e'
+        });
+
+        sdk.create({
+            rating: parseInt($('#rating').val()),
+            description: $('#description').val(),
+            comment: $('#comment').val()
+        })
+        .then((response) => {
+            $('#result').show();
+
+            response.json().then((json) => {
+                $('#result-code span').text(json.meta.code);
+                $('#result-message span').text(json.meta.message);
+
+                if (json.errors) {
+                    $('#result-errors').show();
+                    $('#result-errors span').text(json.errors.join('\n'));
+                }
+                else $('#result-errors').hide();
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+        return false;
     });
 });
