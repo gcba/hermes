b9ecdca2704f117498a7327cdbcb69e5bc68f3a7
Rita Zerrizuela <zeta@widcket.com>
2017-07-22 01:09:29 -0300

    Made it possible to set the port with an environment variable

--- apis/src/ratings/main.go
+++ apis/src/ratings/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 
 	"ratings/controller"
@@ -12,15 +13,31 @@ import (
 )
 
 func main() {
-	port := 5000
+	port, parseErr := strconv.Atoi(os.Getenv("API_RATINGS_PORT"))
+
+	if parseErr != nil {
+		port = 5000
+	}
+
 	routes := map[string]echo.HandlerFunc{
 		"OptionsRoot":    controller.OptionsRoot,
 		"OptionsRatings": controller.OptionsRatings,
 		"PostRatings":    controller.PostRatings}
 
-	handler := handler.Handler(port, routes).(*echo.Echo) // Casting via type assertion
+	handler, castOk := handler.Handler(port, routes).(*echo.Echo)
 
-	fmt.Println("Started server on port", strconv.Itoa(port))
+	if !castOk {
+		handler.Logger.Fatal("Could not start server")
+	}
+
+	banner := `
+_  _ ____ ____ _  _ ____ ____
+|__| |___ |__/ |\/| |___ [__
+|  | |___ |  \ |  | |___ ___] `
+
+	fmt.Println("\n", banner)
+	fmt.Println("\n\n✅  Server started on port", strconv.Itoa(port))
+	fmt.Print("\n\n\033[?25l")
 
 	handler.Logger.Fatal(gracehttp.Serve(handler.Server))
 }
