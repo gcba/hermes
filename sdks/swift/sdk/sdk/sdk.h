a1b2b136d57048b94815097580979efa6bf89f5d
Rita <zeta@widcket.com>
2017-07-25 11:09:25 -0300

    Bootstapped swift framework

---
+++ sdks/swift/sdk/sdk/sdk.h
@@ -0,0 +1,19 @@
+//
+//  sdk.h
+//  sdk
+//
+//  Created by Rita Zerrizuela on 7/25/17.
+//  Copyright © 2017 GCBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sdk.
+FOUNDATION_EXPORT double sdkVersionNumber;
+
+//! Project version string for sdk.
+FOUNDATION_EXPORT const unsigned char sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdk/PublicHeader.h>
+
+
