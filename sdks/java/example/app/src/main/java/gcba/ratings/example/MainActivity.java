132787a33bc4cfe37a035eb90ac5e822bacd0794
Rita <zeta@widcket.com>
2017-07-27 12:45:29 -0300

    Improved example

--- sdks/java/example/app/src/main/java/gcba/ratings/example/MainActivity.java
+++ sdks/java/example/app/src/main/java/gcba/ratings/example/MainActivity.java
@@ -2,6 +2,7 @@ package gcba.ratings.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.json.JSONObject;
 
 import gcba.ratings.sdk.Rating;
 
@@ -22,33 +23,37 @@ public class MainActivity extends AppCompatActivity {
 
                 try{
                     Rating rating = new Rating("https://527ff015.ngrok.io", app, platform, range, token);
+                    JSONObject result;
 
                     // Rating only; no user
 
-                    rating.create(5);
+                    result = rating.create(5);
 
                     // Rating and description only; no user
 
-                    rating.create(4, "Bueno");
+                    result = rating.create(4, "Bueno");
 
                     // Rating, description and comment; no user
 
-                    rating.create(3, "Regular", "Lorem ipsum dolor...");
+                    result = rating.create(3, "Regular", "Lorem ipsum dolor...");
 
                     // Rating, description and comment; user name and mibaId only
 
                     rating.setUser("Juan Pérez", "e10adc394");
-                    rating.create(2, "Regular", "Lorem ipsum dolor...");
+
+                    result = rating.create(2, "Regular", "Lorem ipsum dolor...");
 
                     // Rating, description and comment; user name and email only
 
                     rating.setUser("Juan Pérez", null, "juan@example.com");
-                    rating.create(1, "Muy Malo", "Lorem ipsum dolor...");
+
+                    result = rating.create(1, "Muy Malo", "Lorem ipsum dolor...");
 
                     // Rating, description and comment; user name, email and mibaId
 
                     rating.setUser("Juan Pérez", "e10adc394", "juan@perez.com");
-                    rating.create(5, "Muy Bueno", "Lorem ipsum dolor...");
+
+                    result = rating.create(5, "Muy Bueno", "Lorem ipsum dolor...");
                 } catch(Error e) {
                     System.err.println ("Error: " + e.getMessage());
                 }
