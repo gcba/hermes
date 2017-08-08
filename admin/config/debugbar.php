cb7e69b5a45364acf15307bcab1e0f392ad1b50a
Rita <zeta@widcket.com>
2017-08-08 19:02:09 -0300

    Enabled debugbar

--- admin/config/debugbar.php
+++ admin/config/debugbar.php
@@ -12,8 +12,7 @@ return [
      |
      */
 
-    // 'enabled' => env('DEBUGBAR_ENABLED', null),
-    'enabled' => false,
+    'enabled' => env('DEBUGBAR_ENABLED', null),
 
     /*
      |--------------------------------------------------------------------------
