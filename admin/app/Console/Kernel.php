93db6d176185cc6ec689e4889f8678b715d33f69
Rita <zeta@widcket.com>
2017-08-10 19:10:31 -0300

    Registered commands and service providers

--- admin/app/Console/Kernel.php
+++ admin/app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CreateMailgunRoute;
+
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        CreateMailgunRoute::class
     ];
 
     /**
--- admin/config/app.php
+++ admin/config/app.php
@@ -180,7 +180,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class
+        App\Providers\RouteServiceProvider::class,
+        App\Providers\MailgunServiceProvider::class,
 
     ],
 
