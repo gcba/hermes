b386df9d6388c79c9de436438a1d5e5563e56602
Rita <zeta@widcket.com>
2017-08-10 16:21:37 -0300

    Added Mailgun controller stub

--- admin/app/Http/Controllers/MailgunController.php
+++ admin/app/Http/Controllers/MailgunController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class MailgunController extends Controller
 {
-    //
+     /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+     public function __construct()
+     {
+         $this->middleware('log');
+     }
+
+     /**
+     * Process a Mailgun message.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function __invoke(Request $request)
+    {
+
+
+        return Response::make('', 200);
+    }
 }
