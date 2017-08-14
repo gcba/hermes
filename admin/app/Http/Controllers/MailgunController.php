f8e36d9621f46439888d878d6e61c52825ebcd9c
Rita <zeta@widcket.com>
2017-08-14 20:00:07 -0300

    Completed Mailgun controller

--- admin/app/Http/Controllers/MailgunController.php
+++ admin/app/Http/Controllers/MailgunController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 
@@ -15,7 +17,27 @@ class MailgunController extends Controller
      */
     public function receive(Request $request)
     {
-        // TODO: Parse response
+        $data = [];
+
+        parse_str($request->getContent(), $data);
+
+        $inReplyTo = filter_var(substr(trim($data['In-Reply-To']), 1, -1), FILTER_SANITIZE_EMAIL);
+
+        if (filter_var($inReplyTo, FILTER_VALIDATE_EMAIL)) {
+            $messageReplied = Message::where('transport_id', $inReplyTo)->first();
+
+            if ($messageReplied) {
+                $message = Message::create([
+                    'message' => $data['stripped-text'], // Sanitization happens in mutator
+                    'direction' => 'in',
+                    'notified' => false,
+                    'transport_id' => null,
+                    'rating_id' => $messageReplied->rating->id
+                ]);
+
+                $message->save();
+            }
+        }
 
         return Response::json([], 200);
     }
