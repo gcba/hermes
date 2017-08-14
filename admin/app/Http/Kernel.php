26f2fd5254e0d707df59babc9892ce13edefc70c
Rita <zeta@widcket.com>
2017-08-14 12:47:17 -0300

    Added Mailgun validation middleware

--- admin/app/Http/Kernel.php
+++ admin/app/Http/Kernel.php
@@ -56,5 +56,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'mailgun' => \App\Http\Middleware\ValidateMailgun::class
     ];
 }
