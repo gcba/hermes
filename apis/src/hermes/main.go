85a730d6ae110027c6828454c637e3585d8d363e
Rita <zeta@widcket.com>
2017-08-16 11:43:44 -0300

    Created a command to launch the ratings API

--- apis/src/hermes/main.go
+++ apis/src/hermes/main.go
@@ -8,36 +8,57 @@ import (
 	"hermes/ratings/controller"
 	"hermes/ratings/handler"
 
+	"github.com/alecthomas/kingpin"
 	"github.com/facebookgo/grace/gracehttp"
 	"github.com/labstack/echo"
 )
 
+var (
+	app            = kingpin.New("hermes", "GCBA product ratings APIs.")
+	startCommand   = kingpin.Command("start", "Start an Hermes API.")
+	ratingsCommand = startCommand.Command("ratings", "Start the ratings API.")
+	ratingsPort    = getRatingsPort()
+	noCursor       = "\n\n\033[?25l"
+	banner         = `
+ _  _ ____ ____ _  _ ____ ____
+ |__| |___ |__/ |\/| |___ [__
+ |  | |___ |  \ |  | |___ ___] `
+)
+
 func main() {
-	port, parseErr := strconv.Atoi(os.Getenv("API_RATINGS_PORT"))
+	kingpin.Version("0.0.1")
+	fmt.Println("\n", banner, "\n\n")
 
-	if parseErr != nil {
-		port = 5000
+	switch kingpin.Parse() {
+	case "start ratings":
+		startRatingsAPI()
 	}
+}
 
+func startRatingsAPI() {
 	routes := map[string]echo.HandlerFunc{
 		"OptionsRoot":    controller.OptionsRoot,
 		"OptionsRatings": controller.OptionsRatings,
 		"PostRatings":    controller.PostRatings}
 
-	handler, castOk := handler.Handler(port, routes).(*echo.Echo)
+	handler, castOk := handler.Handler(ratingsPort, routes).(*echo.Echo)
 
 	if !castOk {
 		handler.Logger.Fatal("Could not start server")
 	}
 
-	banner := `
-_  _ ____ ____ _  _ ____ ____
-|__| |___ |__/ |\/| |___ [__
-|  | |___ |  \ |  | |___ ___] `
-
-	fmt.Println("\n", banner)
-	fmt.Println("\n\n✅  Server started on port", strconv.Itoa(port))
-	fmt.Print("\n\n\033[?25l")
+	fmt.Println("✅  Server started on port", strconv.Itoa(ratingsPort))
+	fmt.Print(noCursor)
 
 	handler.Logger.Fatal(gracehttp.Serve(handler.Server))
+}
+
+func getRatingsPort() int {
+	port, portErr := strconv.Atoi(os.Getenv("API_RATINGS_PORT"))
+
+	if portErr != nil {
+		return 5000
+	}
+
+	return port
 }
