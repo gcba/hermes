1d8890b7dc88027b616bc6a6aa1dcf8f475378bd
Rita <zeta@widcket.com>
2017-08-23 10:15:23 -0300

    Updated schema

--- apis/src/hermes/stats/schema/resolvers.go
+++ apis/src/hermes/stats/schema/resolvers.go
@@ -12,10 +12,7 @@ type (
 	field struct {
 		Name     string
 		Operator *string
-		Int      *int32
-		Float    *float64
-		String   *string
-		Bool     *bool
+		Value    Value
 		Next     *operation
 	}
 
@@ -27,47 +24,19 @@ type (
 	Resolver struct {
 		db *gorm.DB
 	}
-
-	CountResolver struct {
-		db *gorm.DB
-	}
-
-	AverageResolver struct {
-		db *gorm.DB
-	}
 )
 
 func errorResponse() error {
 	return echo.NewHTTPError(http.StatusInternalServerError)
 }
 
-func (r *Resolver) Count(context context.Context) (*CountResolver, error) {
-	if db, ok := context.Value(DB).(*gorm.DB); ok {
-		// TODO: Implement
-
-		return &CountResolver{db: db}, nil
-	}
-
-	return nil, errorResponse()
-}
-
-func (r *Resolver) Average(context context.Context) (*AverageResolver, error) {
-	if db, ok := context.Value(DB).(*gorm.DB); ok {
-		// TODO: Implement
-
-		return &AverageResolver{db: db}, nil
-	}
-
-	return nil, errorResponse()
-}
-
-func (cr *CountResolver) Ratings(context context.Context, args struct{ Field *field }) (int32, error) {
+func (r *Resolver) Count(context context.Context, args struct{ Field *field }) (int32, error) {
 	// TODO: Implement
 
 	return 0, nil
 }
 
-func (ar *AverageResolver) Ratings(context context.Context, args struct{ Field *field }) (float64, error) {
+func (r *Resolver) Average(context context.Context, args struct{ Field *field }) (float64, error) {
 	// TODO: Implement
 
 	return 0, nil
--- apis/src/hermes/stats/schema/schema.go
+++ apis/src/hermes/stats/schema/schema.go
@@ -1,6 +1,7 @@
 package schema
 
 import (
+	"errors"
 	"io/ioutil"
 	"path"
 	"runtime"
@@ -13,6 +14,34 @@ import (
 const DB = iota
 
 var Schema *graphql.Schema
+
+type Value struct {
+	String string
+	Int    int
+	Float  float64
+	Bool   bool
+}
+
+func (_ Value) ImplementsGraphQLType(name string) bool {
+	return name == "Value"
+}
+
+func (v *Value) UnmarshalGraphQL(input interface{}) error {
+	switch input := input.(type) {
+	case string:
+		v.String = input
+	case int:
+		v.Int = input
+	case float64:
+		v.Float = input
+	case bool:
+		v.Bool = input
+	default:
+		return errors.New("Wrong type")
+	}
+
+	return nil
+}
 
 func Parse() {
 	var rawSchema []byte
