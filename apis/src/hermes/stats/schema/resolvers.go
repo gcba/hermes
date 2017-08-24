ccd149f31b2ff02a28eaafb8ad6c6dd261c4a5ec
Rita Zerrizuela <zeta@widcket.com>
2017-08-24 15:20:40 -0300

    Implemented average

--- apis/src/hermes/stats/schema/resolvers.go
+++ apis/src/hermes/stats/schema/resolvers.go
@@ -44,11 +44,7 @@ func (r *Resolver) Count(context context.Context, args arguments) (int32, error)
 		errorList := query.GetErrors()
 
 		if !(len(errorList) > 0 || query.Error != nil || query.Value == nil) {
-			var count int32
-
-			query.Count(&count)
-
-			total += count
+			query.Count(&total)
 		}
 	}
 
@@ -58,9 +54,20 @@ func (r *Resolver) Count(context context.Context, args arguments) (int32, error)
 }
 
 func (r *Resolver) Average(context context.Context, args arguments) (float64, error) {
-	// TODO: Implement
+	var total float64
 
-	return 0, nil
+	if db, castOk := context.Value(DB).(*gorm.DB); castOk {
+		entity := args.Field.getEntity()
+		average := fmt.Sprintf("AVG(%s)", entity.Field)
+		query := args.Field.query(db).Select(average)
+		errorList := query.GetErrors()
+
+		if !(len(errorList) > 0 || query.Error != nil || query.Value == nil) {
+			query.Row().Scan(&total)
+		}
+	}
+
+	return total, nil
 }
 
 func (f *field) query(db *gorm.DB) *gorm.DB {
@@ -71,25 +78,25 @@ func (f *field) query(db *gorm.DB) *gorm.DB {
 
 	switch entity.Table {
 	case "apps":
-		return db.Where(where, value).Find(&[]models.App{})
+		return db.Model(&models.Rating{}).Where(where, value)
 	case "appusers":
-		return db.Where(where, value).Find(&[]models.AppUser{})
+		return db.Model(&models.AppUser{}).Where(where, value)
 	case "brands":
-		return db.Where(where, value).Find(&[]models.Brand{})
+		return db.Model(&models.Brand{}).Where(where, value)
 	case "browsers":
-		return db.Where(where, value).Find(&[]models.Browser{})
+		return db.Model(&models.Browser{}).Where(where, value)
 	case "devices":
-		return db.Where(where, value).Find(&[]models.Device{})
+		return db.Model(&models.Device{}).Where(where, value)
 	case "messages":
-		return db.Where(where, value).Find(&[]models.Message{})
+		return db.Model(&models.Message{}).Where(where, value)
 	case "platforms":
-		return db.Where(where, value).Find(&[]models.Platform{})
+		return db.Model(&models.Platform{}).Where(where, value)
 	case "ranges":
-		return db.Where(where, value).Find(&[]models.Range{})
+		return db.Model(&models.Range{}).Where(where, value)
 	case "ratings":
 		fallthrough
 	default:
-		return db.Where(where, value).Find(&[]models.Rating{})
+		return db.Model(&models.Rating{}).Where(where, value)
 	}
 }
 
