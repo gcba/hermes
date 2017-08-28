cf0984e8f9a80b07f06374dec12f44b42370f64f
Rita <zeta@widcket.com>
2017-08-28 11:52:51 -0300

    Added new controller stub

---
+++ admin/app/Http/Controllers/ApiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use TCG\Voyager\Facades\Voyager;
+use Yajra\Datatables\Datatables;
+use App\Rating;
+use App\Message;
+use App\Device;
+use App\AppUser;
+
+class ApiController extends Controller {}
