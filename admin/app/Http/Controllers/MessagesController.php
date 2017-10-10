2f5cc41e397d52f79c738ea156c0b1d9c9755786
Rita <zeta@widcket.com>
2017-10-10 10:31:57 -0300

    Added basic MessagesController implementation

--- admin/app/Http/Controllers/MessagesController.php
+++ admin/app/Http/Controllers/MessagesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\AppUser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use TCG\Voyager\Facades\Voyager;
 use Validator;
 
@@ -13,8 +15,20 @@ class MessagesController extends Controller
     // POST BRE(A)D
     public function store(Request $request)
     {
-        parent::store($request);
+        $response = parent::store($request);
+        $text = $request->input('message');
+        $userId = $request->input('user');
+        $subject = $request->input('input');
+        $user = DB::table('appusers')->where('miba_id', $userId)->first();
 
+        if (isset($user->email)) {
+            $result = Mailgun::raw($text, function ($message, $subject) use($user) {
+                $message->to($user->email, env('MAILGUN_SENDER', ''))->subject($subject);
 
+                return;
+            });
+        }
+
+        return $response;
     }
 }
