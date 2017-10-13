6d18991d396db0923289ee4746e8400331272952
Rita <zeta@widcket.com>
2017-10-13 14:10:02 -0300

    Updated build script

--- sdks/js/build.js
+++ sdks/js/build.js
@@ -8,7 +8,6 @@ const babel = require('rollup-plugin-babel');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 const del = require('del');
-const pkg = require('./package.json');
 
 const resolvePlugin = nodeResolve({
     jsnext: true, // Default: false
@@ -78,14 +77,5 @@ for (const config of bundles) {
         promise = promise.then(() => rollup.rollup(rollupConfig).then(bundle => bundle.write(exampleConfig)));
     }
 }
-
-promise = promise.then(() => {
-    delete pkg.devDependencies;
-    delete pkg.scripts;
-    delete pkg.eslintConfig;
-
-    fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
-    fs.writeFileSync('dist/LICENSE.md', fs.readFileSync('LICENSE.md', 'utf-8'), 'utf-8');
-});
 
 promise.catch(err => console.error(err.stack)); // eslint-disable-line no-console
