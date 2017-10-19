b5022e9469a7ca2604ff624d1bcfff7dfc9478f1
Rita <zeta@widcket.com>
2017-10-19 16:37:29 -0300

    Set messages as notified

--- admin/app/Http/Controllers/ApiController.php
+++ admin/app/Http/Controllers/ApiController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Response;
+use App\Jobs\SetMessageStatus;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 use TCG\Voyager\Facades\Voyager;
 use Yajra\Datatables\Datatables;
 use App\Message;
@@ -37,6 +38,12 @@ class ApiController extends Controller {
 
         if ($user->hasPermission('read_messages')) {
             $messages = Message::where('rating_id', $id)->orderBy('created_at', 'asc')->get();
+
+             foreach ($messages as $item) {
+                if ($item->direction === 'in' && $item->status === 0) {
+                    SetMessageStatus::dispatch($item, 1);
+                }
+            }
 
             return Response::json($messages);
         }
