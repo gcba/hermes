9f7b4ca617b4f5de2e78d83e26151da7f87e72c3
Rita <zeta@widcket.com>
2017-11-03 18:08:34 -0300

    Added admin pass-thru

--- admin/app/Policies/AppPolicy.php
+++ admin/app/Policies/AppPolicy.php
@@ -7,6 +7,12 @@ use TCG\Voyager\Contracts\User as UserType;
 
 class AppPolicy extends BasePolicy
 {
+    public function before($user, $ability) {
+        if ($user->role()->pluck('name')->toArray()[0] === 'admin') {
+            return true;
+        }
+    }
+
     protected function checkApp(UserType $user, $model) {
         $userApps = $user->apps()->pluck('id')->toArray();
 
--- admin/app/Policies/PlatformPolicy.php
+++ admin/app/Policies/PlatformPolicy.php
@@ -7,6 +7,12 @@ use TCG\Voyager\Contracts\User as UserType;
 
 class PlatformPolicy extends BasePolicy
 {
+    public function before($user, $ability) {
+        if ($user->role()->pluck('name')->toArray()[0] === 'admin') {
+            return true;
+        }
+    }
+
     protected function checkApp(UserType $user, $model) {
         $userApps = $user->apps()->pluck('id')->toArray();
         $platformApps = $model->apps()->pluck('id')->toArray();
