7542a6bc57fd8c765de5c6799f5116be90990f38
Rita Zerrizuela <zeta@widcket.com>
2017-11-03 22:58:00 -0300

    Added dashboard custom view

---
+++ admin/resources/views/vendor/voyager/index.blade.php
@@ -0,0 +1,11 @@
+@extends('voyager::master')
+
+@section('content')
+    <div class="page-content">
+        @include('voyager::alerts')
+        @include('voyager::dimmers')
+        <div class="analytics-container">
+
+        </div>
+    </div>
+@stop
