1dc5ad2448fa685d31531d22d30951d88e666c23
Rita <zeta@widcket.com>
2017-11-06 11:46:16 -0300

    Avoided deleting default avatar when deleting a user

--- admin/app/Http/Controllers/Controller.php
+++ admin/app/Http/Controllers/Controller.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use TCG\Voyager\Facades\Voyager;
 use TCG\Voyager\Http\Controllers\VoyagerBreadController as BreadController;
+use TCG\Voyager\Events\BreadImagesDeleted;
 use Yajra\DataTables\Datatables;
 use Validator;
 
 class Controller extends BreadController
 {
     // From Voyager's VoyagerBreadController.php, customized
-
     // POST BRE(A)D
     public function store(Request $request)
     {
@@ -41,7 +41,6 @@ class Controller extends BreadController
     }
 
     // From Voyager's VoyagerBreadController.php, customized
-
     // POST BR(E)AD
     public function update(Request $request, $id)
     {
@@ -69,6 +68,43 @@ class Controller extends BreadController
                     'message'    => "Ítem editado exitosamente",
                     'alert-type' => 'success',
                 ]);
+        }
+    }
+
+    // From Voyager's VoyagerBreadController.php, customized
+    /**
+     * Delete all images related to a BREAD item.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $data
+     * @param \Illuminate\Database\Eloquent\Model $rows
+     *
+     * @return void
+     */
+    public function deleteBreadImages($data, $rows)
+    {
+        foreach ($rows as $row) {
+            if ($data->{$row->field} !== config('voyager.user.default_avatar', 'users/default.png')) {
+                $this->deleteFileIfExists($data->{$row->field});
+
+                $options = json_decode($row->details);
+
+                if (isset($options->thumbnails)) {
+                    foreach ($options->thumbnails as $thumbnail) {
+                        $ext = explode('.', $data->{$row->field});
+                        $extension = '.'.$ext[count($ext) - 1];
+
+                        $path = str_replace($extension, '', $data->{$row->field});
+
+                        $thumb_name = $thumbnail->name;
+
+                        $this->deleteFileIfExists($path.'-'.$thumb_name.$extension);
+                    }
+                }
+            }
+        }
+
+        if ($rows->count() > 0) {
+            event(new BreadImagesDeleted($data, $rows));
         }
     }
 }
