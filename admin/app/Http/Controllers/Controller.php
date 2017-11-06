445063cb961c517e8dfd109952ba6d2e1822ee36
Rita <zeta@widcket.com>
2017-11-06 12:26:12 -0300

    Removed overrides

--- admin/app/Http/Controllers/Controller.php
+++ admin/app/Http/Controllers/Controller.php
@@ -12,66 +12,6 @@ use Validator;
 class Controller extends BreadController
 {
     // From Voyager's VoyagerBreadController.php, customized
-    // POST BRE(A)D
-    public function store(Request $request)
-    {
-        $slug = $this->getSlug($request);
-        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
-
-        // Check permission
-        Voyager::canOrFail('add_'.$dataType->name);
-
-        //Validate fields with ajax
-        $val = $this->validateBread($request->all(), $dataType->addRows);
-
-        if ($val->fails()) {
-            return response()->json(['errors' => $val->messages()]);
-        }
-
-        if (!$request->ajax()) {
-            $data = $this->insertUpdateData($request, $slug, $dataType->addRows, new $dataType->model_name());
-
-            return redirect()
-                ->route("voyager.{$dataType->slug}.index", ['id' => $data->id])
-                ->with([
-                        'message'    => "Ítem creado exitosamente",
-                        'alert-type' => 'success',
-                    ]);
-        }
-    }
-
-    // From Voyager's VoyagerBreadController.php, customized
-    // POST BR(E)AD
-    public function update(Request $request, $id)
-    {
-        $slug = $this->getSlug($request);
-        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
-
-        // Check permission
-        Voyager::canOrFail('edit_'.$dataType->name);
-
-        //Validate fields with ajax
-        $val = $this->validateBread($request->all(), $dataType->editRows);
-
-        if ($val->fails()) {
-            return response()->json(['errors' => $val->messages()]);
-        }
-
-        if (!$request->ajax()) {
-            $data = call_user_func([$dataType->model_name, 'findOrFail'], $id);
-
-            $this->insertUpdateData($request, $slug, $dataType->editRows, $data, $id);
-
-            return redirect()
-                ->route("voyager.{$dataType->slug}.index", ['id' => $id])
-                ->with([
-                    'message'    => "Ítem editado exitosamente",
-                    'alert-type' => 'success',
-                ]);
-        }
-    }
-
-    // From Voyager's VoyagerBreadController.php, customized
     /**
      * Delete all images related to a BREAD item.
      *
